604bc473ea0343682a3944f94b6bc774e518db5d
fengweibin <tx_xinxin2006@163.com>
2018-06-13 11:00:04 +0800

    理论课程信息

---
+++ teachingplan-parent/educational-teachingplan-entities/src/main/java/com/kalix/educational/teachingplan/entities/LessonInfoBean.java
@@ -0,0 +1,401 @@
+package com.kalix.educational.teachingplan.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+/**
+ * Created by Administrator_ on 2018/6/13.
+ */
+
+@Entity
+@Table(name = "educational_lesson_info")
+public class LessonInfoBean {
+
+    private String ZY_FLAG;
+    private String DM;
+    private String QYKCDM;
+    private String ZWMC;
+    private String ZWJC;
+    private String YWMC;
+    private String YWJC;
+    private String CDDW_ID;
+    private Double ZXF;
+    private Double ZXS;
+    private Integer SYXS;
+    private String XXKC;
+    private String LRJJ;
+    private String SYZY;
+    private String CKJC;
+    private String ZGR;
+    private String CBDW;
+    private Integer YS;
+    private Double DJ;
+    private String ZB;
+    private String SHR;
+    private String kclb;
+    private String sjlb;
+    private String sxlb;
+    private Integer sjxs;
+    private Integer qtxs;
+    private String p_flag;
+    private String user_kcid;
+    private String xsfpxl;
+    private String kcz;
+    private String bycz_flag;
+    private String ljdz;
+    private String pycc;
+    private String xk;
+    private Integer jys;
+    private Integer JPKCLB;
+    private String QXHZKFKC;
+    private String ZYKSSJXM;
+    private String lb1_id;
+    private String lb2_id;
+    private String au_ftype;
+    private String au_state;
+    private String au_updatetime;
+
+    public String getZY_FLAG() {
+        return ZY_FLAG;
+    }
+
+    public void setZY_FLAG(String ZY_FLAG) {
+        this.ZY_FLAG = ZY_FLAG;
+    }
+
+    public String getDM() {
+        return DM;
+    }
+
+    public void setDM(String DM) {
+        this.DM = DM;
+    }
+
+    public String getQYKCDM() {
+        return QYKCDM;
+    }
+
+    public void setQYKCDM(String QYKCDM) {
+        this.QYKCDM = QYKCDM;
+    }
+
+    public String getZWMC() {
+        return ZWMC;
+    }
+
+    public void setZWMC(String ZWMC) {
+        this.ZWMC = ZWMC;
+    }
+
+    public String getZWJC() {
+        return ZWJC;
+    }
+
+    public void setZWJC(String ZWJC) {
+        this.ZWJC = ZWJC;
+    }
+
+    public String getYWMC() {
+        return YWMC;
+    }
+
+    public void setYWMC(String YWMC) {
+        this.YWMC = YWMC;
+    }
+
+    public String getYWJC() {
+        return YWJC;
+    }
+
+    public void setYWJC(String YWJC) {
+        this.YWJC = YWJC;
+    }
+
+    public String getCDDW_ID() {
+        return CDDW_ID;
+    }
+
+    public void setCDDW_ID(String CDDW_ID) {
+        this.CDDW_ID = CDDW_ID;
+    }
+
+    public Double getZXF() {
+        return ZXF;
+    }
+
+    public void setZXF(Double ZXF) {
+        this.ZXF = ZXF;
+    }
+
+    public Double getZXS() {
+        return ZXS;
+    }
+
+    public void setZXS(Double ZXS) {
+        this.ZXS = ZXS;
+    }
+
+    public Integer getSYXS() {
+        return SYXS;
+    }
+
+    public void setSYXS(Integer SYXS) {
+        this.SYXS = SYXS;
+    }
+
+    public String getXXKC() {
+        return XXKC;
+    }
+
+    public void setXXKC(String XXKC) {
+        this.XXKC = XXKC;
+    }
+
+    public String getLRJJ() {
+        return LRJJ;
+    }
+
+    public void setLRJJ(String LRJJ) {
+        this.LRJJ = LRJJ;
+    }
+
+    public String getSYZY() {
+        return SYZY;
+    }
+
+    public void setSYZY(String SYZY) {
+        this.SYZY = SYZY;
+    }
+
+    public String getCKJC() {
+        return CKJC;
+    }
+
+    public void setCKJC(String CKJC) {
+        this.CKJC = CKJC;
+    }
+
+    public String getZGR() {
+        return ZGR;
+    }
+
+    public void setZGR(String ZGR) {
+        this.ZGR = ZGR;
+    }
+
+    public String getCBDW() {
+        return CBDW;
+    }
+
+    public void setCBDW(String CBDW) {
+        this.CBDW = CBDW;
+    }
+
+    public Integer getYS() {
+        return YS;
+    }
+
+    public void setYS(Integer YS) {
+        this.YS = YS;
+    }
+
+    public Double getDJ() {
+        return DJ;
+    }
+
+    public void setDJ(Double DJ) {
+        this.DJ = DJ;
+    }
+
+    public String getZB() {
+        return ZB;
+    }
+
+    public void setZB(String ZB) {
+        this.ZB = ZB;
+    }
+
+    public String getSHR() {
+        return SHR;
+    }
+
+    public void setSHR(String SHR) {
+        this.SHR = SHR;
+    }
+
+    public String getKclb() {
+        return kclb;
+    }
+
+    public void setKclb(String kclb) {
+        this.kclb = kclb;
+    }
+
+    public String getSjlb() {
+        return sjlb;
+    }
+
+    public void setSjlb(String sjlb) {
+        this.sjlb = sjlb;
+    }
+
+    public String getSxlb() {
+        return sxlb;
+    }
+
+    public void setSxlb(String sxlb) {
+        this.sxlb = sxlb;
+    }
+
+    public Integer getSjxs() {
+        return sjxs;
+    }
+
+    public void setSjxs(Integer sjxs) {
+        this.sjxs = sjxs;
+    }
+
+    public Integer getQtxs() {
+        return qtxs;
+    }
+
+    public void setQtxs(Integer qtxs) {
+        this.qtxs = qtxs;
+    }
+
+    public String getP_flag() {
+        return p_flag;
+    }
+
+    public void setP_flag(String p_flag) {
+        this.p_flag = p_flag;
+    }
+
+    public String getUser_kcid() {
+        return user_kcid;
+    }
+
+    public void setUser_kcid(String user_kcid) {
+        this.user_kcid = user_kcid;
+    }
+
+    public String getXsfpxl() {
+        return xsfpxl;
+    }
+
+    public void setXsfpxl(String xsfpxl) {
+        this.xsfpxl = xsfpxl;
+    }
+
+    public String getKcz() {
+        return kcz;
+    }
+
+    public void setKcz(String kcz) {
+        this.kcz = kcz;
+    }
+
+    public String getBycz_flag() {
+        return bycz_flag;
+    }
+
+    public void setBycz_flag(String bycz_flag) {
+        this.bycz_flag = bycz_flag;
+    }
+
+    public String getLjdz() {
+        return ljdz;
+    }
+
+    public void setLjdz(String ljdz) {
+        this.ljdz = ljdz;
+    }
+
+    public String getPycc() {
+        return pycc;
+    }
+
+    public void setPycc(String pycc) {
+        this.pycc = pycc;
+    }
+
+    public String getXk() {
+        return xk;
+    }
+
+    public void setXk(String xk) {
+        this.xk = xk;
+    }
+
+    public Integer getJys() {
+        return jys;
+    }
+
+    public void setJys(Integer jys) {
+        this.jys = jys;
+    }
+
+    public Integer getJPKCLB() {
+        return JPKCLB;
+    }
+
+    public void setJPKCLB(Integer JPKCLB) {
+        this.JPKCLB = JPKCLB;
+    }
+
+    public String getQXHZKFKC() {
+        return QXHZKFKC;
+    }
+
+    public void setQXHZKFKC(String QXHZKFKC) {
+        this.QXHZKFKC = QXHZKFKC;
+    }
+
+    public String getZYKSSJXM() {
+        return ZYKSSJXM;
+    }
+
+    public void setZYKSSJXM(String ZYKSSJXM) {
+        this.ZYKSSJXM = ZYKSSJXM;
+    }
+
+    public String getLb1_id() {
+        return lb1_id;
+    }
+
+    public void setLb1_id(String lb1_id) {
+        this.lb1_id = lb1_id;
+    }
+
+    public String getLb2_id() {
+        return lb2_id;
+    }
+
+    public void setLb2_id(String lb2_id) {
+        this.lb2_id = lb2_id;
+    }
+
+    public String getAu_ftype() {
+        return au_ftype;
+    }
+
+    public void setAu_ftype(String au_ftype) {
+        this.au_ftype = au_ftype;
+    }
+
+    public String getAu_state() {
+        return au_state;
+    }
+
+    public void setAu_state(String au_state) {
+        this.au_state = au_state;
+    }
+
+    public String getAu_updatetime() {
+        return au_updatetime;
+    }
+
+    public void setAu_updatetime(String au_updatetime) {
+        this.au_updatetime = au_updatetime;
+    }
+}
