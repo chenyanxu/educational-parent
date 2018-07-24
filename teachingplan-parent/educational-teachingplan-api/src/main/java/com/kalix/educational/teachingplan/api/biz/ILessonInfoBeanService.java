1b0edd4f77e1c5dca052b1b26af5054d25bcc51d
fengweibin <tx_xinxin2006@163.com>
2018-07-24 08:39:23 +0800

    课程信息

---
+++ teachingplan-parent/educational-teachingplan-api/src/main/java/com/kalix/educational/teachingplan/api/biz/ILessonInfoBeanService.java
@@ -0,0 +1,12 @@
+package com.kalix.educational.teachingplan.api.biz;
+
+import com.kalix.educational.teachingplan.entities.LessonInfoBean;
+import com.kalix.framework.core.api.biz.IBizService;
+
+
+/**
+ * Created by dell on 14-1-17.
+ */
+public interface ILessonInfoBeanService extends IBizService<LessonInfoBean> {
+   // JsonStatus resetHardwareinfo(Long id);
+}
---
+++ teachingplan-parent/educational-teachingplan-dao/src/main/java/com/kalix/educational/teachingplan/dao/LessonInfoBeanDaoImpl.java
@@ -0,0 +1,15 @@
+package com.kalix.educational.teachingplan.dao;
+
+import com.kalix.educational.teachingplan.api.dao.ILessonInfoBeanDao;
+import com.kalix.educational.teachingplan.entities.LessonInfoBean;;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+
+/**
+ * Created by Administrator on 2017/3/3.
+ */
+public class LessonInfoBeanDaoImpl extends BaseAdminDao<LessonInfoBean, Long> implements ILessonInfoBeanDao {
+
+}
