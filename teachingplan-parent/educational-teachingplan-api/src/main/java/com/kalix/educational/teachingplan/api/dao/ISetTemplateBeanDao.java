530e473b17bd8581e0635f0559bdf9e699e9c41d
houqingjie <150091225@qq.com>
2018-08-23 15:04:11 +0800

    移除理论课程和实践环节服务,调整毕业学分要求模板

--- teachingplan-parent/educational-teachingplan-api/src/main/java/com/kalix/educational/teachingplan/api/dao/ISetTemplateBeanDao.java
+++ teachingplan-parent/educational-teachingplan-api/src/main/java/com/kalix/educational/teachingplan/api/dao/ISetTemplateBeanDao.java
@@ -1,13 +1,10 @@
 package com.kalix.educational.teachingplan.api.dao;
 
-
 import com.kalix.educational.teachingplan.entities.SetTemplateBean;
 import com.kalix.framework.core.api.dao.IGenericDao;
-
 
 /**
  * Created by Administrator on 2017/3/3.
  */
 public interface ISetTemplateBeanDao extends IGenericDao<SetTemplateBean,Long> {
-
 }
