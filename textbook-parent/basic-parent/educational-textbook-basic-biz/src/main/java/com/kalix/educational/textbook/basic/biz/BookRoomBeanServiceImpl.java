b9d18b96e622ff0513ed536683d762a23c7bbcd1
zangyanming <zyanming2002@163.com>
2018-08-24 10:12:24 +0800

    增加了字典类型翻译

--- textbook-parent/basic-parent/educational-textbook-basic-biz/src/main/java/com/kalix/educational/textbook/basic/biz/BookRoomBeanServiceImpl.java
+++ textbook-parent/basic-parent/educational-textbook-basic-biz/src/main/java/com/kalix/educational/textbook/basic/biz/BookRoomBeanServiceImpl.java
@@ -4,7 +4,11 @@ package com.kalix.educational.textbook.basic.biz;
 import com.kalix.educational.textbook.basic.api.biz.IBookRoomBeanService;
 import com.kalix.educational.textbook.basic.api.dao.IBookRoomBeanDao;
 import com.kalix.educational.textbook.basic.entities.BookRoomBean;
+import com.kalix.educational.textbook.basic.entities.BookShelfBean;
+import com.kalix.framework.core.api.persistence.JsonData;
 import com.kalix.framework.core.impl.biz.ShiroGenericBizServiceImpl;
+import com.kalix.general.org.api.biz.ISchoolZoneBeanService;
+import com.kalix.general.org.entities.SchoolZoneBean;
 
 
 /**
@@ -12,8 +16,30 @@ import com.kalix.framework.core.impl.biz.ShiroGenericBizServiceImpl;
  */
 public class BookRoomBeanServiceImpl extends ShiroGenericBizServiceImpl<IBookRoomBeanDao, BookRoomBean> implements IBookRoomBeanService {
 
+    private ISchoolZoneBeanService schoolZoneBeanService;
+
+    @Override
+    public JsonData getAllEntityByQuery(Integer page, Integer limit, String jsonStr, String sort) {
+        JsonData jsonData = super.getAllEntityByQuery(page, limit, jsonStr, sort);
+        for (int i = 0; i < jsonData.getData().size(); i++) {
+            BookRoomBean bookRoomBean = (BookRoomBean) jsonData.getData().get(i);
+            SchoolZoneBean schoolZoneBean = schoolZoneBeanService.getEntity(bookRoomBean.getSarea());
+            bookRoomBean.setSareaName(schoolZoneBean.getZwmc());
+        }
+
+        return jsonData;
+    }
+
+    @Override
+    public BookRoomBean getEntity(long entityId) {
+        BookRoomBean bookRoomBean = super.getEntity(entityId);
+        SchoolZoneBean schoolZoneBean = schoolZoneBeanService.getEntity(bookRoomBean.getSarea());
+        bookRoomBean.setSareaName(schoolZoneBean.getZwmc());
+        return bookRoomBean;
+    }
 
 
-
-
+    public void setSchoolZoneBeanService(ISchoolZoneBeanService schoolZoneBeanService) {
+        this.schoolZoneBeanService = schoolZoneBeanService;
+    }
 }
